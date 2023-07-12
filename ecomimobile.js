416ef0bf4caadc9cf1ac1bb89dfa2ac090f8052d
Arthur Polizér <arthur_polizer@live.com>
2023-07-12 18:49:27 -0400

    atividade mobile

---
+++ ecomimobile.js
@@ -0,0 +1,22 @@
+class Contafisica{
+    constructor(pNome, pCpf,pDatanascimento, pEndereco){
+        this.Nome = pNome;
+        this.Cpf = pCpf;
+        this.Datanascimento = pDatanascimento;
+        this.Endereco = pEndereco
+     }
+    imprimir(){
+    }
+    }
+
+class Contajuridida{
+    constructor(pNome, pCpf,pRazaosocial, pEndereco){
+        this.Nome = pNome;
+        this.Cpf = pCpf;
+        this.Razaosocial = pRazaosocial;
+        this.Endereco = pEndereco;
+     }
+     imprimir(){
+     }
+     }
+     
